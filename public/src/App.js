692fc80e8a6c017795d91876ec894d891195d02d
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-04-29 14:37:37 -0700

    Set default state vals for auth

--- public/src/App.js
+++ public/src/App.js
@@ -2,25 +2,35 @@ import React, { Component } from 'react';
 import SongList from './SongList/SongList';
 import NavBar from './NavBar/NavBar';
 import JamView from './Jam/JamView';
+import { Grid, Row, Col } from 'react-bootstrap';
+
 
 class App extends Component {
   constructor(){
     super();
 
     this.state = {
-
+      isLoggedIn: false,
+      currentUser: '',
     };
   }
-
-
 
   render(){
     return(
       <div>
-        <p>In The App Component</p>
-        <NavBar />
-        <SongList />
-        <JamView />
+        <Grid>
+          <Row xs={12} md={0} mdPull={0}>
+            <Col>
+              <NavBar />
+            </Col>
+          </Row>
+          <Row xs={8} md={2} mdPull={2}>
+            <Col>
+              <SongList />
+              <JamView />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
