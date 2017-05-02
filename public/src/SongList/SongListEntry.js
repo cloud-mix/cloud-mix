0d36bc3913e719e4292ea0a186d15222af76a6e9
David de Sousa <admin@anatta.local>
2017-05-02 15:02:26 -0700

    Makes progress on songentryview

--- public/src/SongList/SongListEntry.js
+++ public/src/SongList/SongListEntry.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Card, CardTitle } from "react-materialize";
+import { Col } from 'react-bootstrap';
 
 var SongListEntry = function(props) {
   return (
-    <div>
-      <p>In the SongListEntry Component</p>
-    </div>
+    <Card
+      className="small"
+      header={
+        <CardTitle image="https://res.cloudinary.com/demo/video/upload/h_200,w_500,fl_waveform,co_black,b_white/bumblebee.png">
+          Example Song
+        </CardTitle>
+      }
+      actions={[<a href="#">Creator</a>]}
+    >
+      Genre: Example
+    </Card>
   );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const SRC_DIR = path.resolve(__dirname, 'public');
-const BUILD_DIR = path.resolve(__dirname, 'public/bundle');
+const path = require("path");
+const SRC_DIR = path.resolve(__dirname, "public");
+const BUILD_DIR = path.resolve(__dirname, "public/bundle");
 
 module.exports = {
-  entry: path.resolve(SRC_DIR, 'index.js'),
+  entry: path.resolve(SRC_DIR, "index.js"),
   output: {
-    filename: 'bundle.js',
+    filename: "bundle.js",
     path: BUILD_DIR
   },
   module: {
@@ -13,16 +13,27 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: [/node_modules/],
-        use: [{
-          loader: 'babel-loader',
-          options: { presets: ['es2015', 'react', 'stage-1'] }
-        }],
+        use: [
+          {
+            loader: "babel-loader",
+            options: { presets: ["es2015", "react", "stage-1"] }
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|ico)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: { query: { name: "images/[name].[ext]" } }
+          }
+        ]
       }
     ],
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
-      {test: /\.css$/, loader: "style-loader!css-loader"}
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.css$/, loader: "style-loader!css-loader" }
     ]
   },
-  devtool: "inline-source-map",
-}
+  devtool: "inline-source-map"
+};
