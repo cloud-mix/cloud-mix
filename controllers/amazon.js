07806b14c2f12c4d335bb8e26f8c124f47f4f497
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-05-01 18:35:37 -0700

    AWS S3 upload works with postman and node server side

---
+++ controllers/amazon.js
@@ -0,0 +1,48 @@
+var s3 = require("s3");
+
+var client = s3.createClient({
+ maxAsyncS3: 20,     // this is the default
+ s3RetryCount: 3,    // this is the default
+ s3RetryDelay: 1000, // this is the default
+ multipartUploadThreshold: 20971520, // this is the default (20 MB)
+ multipartUploadSize: 15728640, // this is the default (15 MB)
+ s3Options: {
+   accessKeyId: process.env.AWSAccessKeyId,
+   secretAccessKey: process.env.AWSSecretKey,
+ },
+});
+
+
+var uploader = function (req, res) {
+
+  var file = req.body.file;
+  var zeKey = req.body.key;
+
+  var params = {
+    localFile: file,
+
+    s3Params: {
+      Bucket: "cloudmix",
+      Key: zeKey
+    },
+  };
+
+  var upload = client.uploadFile(params);
+    upload.on('error', function(err) {
+     console.error("unable to upload: ", err.stack);
+    });
+    upload.on('progress', function() {
+     console.log("progress", upload.progressMd5Amount,
+               upload.progressAmount, upload.progressTotal);
+    });
+    upload.on("end", function() {
+     console.log("done uploading");
+    });
+
+}
+
+
+
+module.exports = {
+  uploader
+}
