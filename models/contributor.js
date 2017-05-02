c424f662238a6c0df3ed2e18143ff12721dde06c
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-05-02 16:47:18 -0700

    update models might need slef join for contribs and users

--- models/contributor.js
+++ models/contributor.js
@@ -4,7 +4,6 @@ var Users = require('./user');
 var Songs = require('./song');
 
 var Contributor = db.define('contributor', {
-  id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
   username: Sequelize.TEXT,
   track: Sequelize.TEXT
 });
