80012d3c5ce8803ed4d068e9078b2759148ade2f
David de Sousa <admin@anatta.local>
2017-05-04 01:25:21 -0700

    Code variations for s3 uploading

--- controllers/amazon.js
+++ controllers/amazon.js
@@ -1,69 +1,51 @@
-var s3 = require("s3");
+var AWS = require("aws-sdk");
 var fs = require("fs");
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var atob = require("atob");
+// Load credentials and set region from JSON file
+AWS.config.loadFromPath("./config.json");
 var FileReader = require("filereader");
-
-var client = s3.createClient({
-  maxAsyncS3: 20, // this is the default
-  s3RetryCount: 3, // this is the default
-  s3RetryDelay: 1000, // this is the default
-  multipartUploadThreshold: 20971520, // this is the default (20 MB)
-  multipartUploadSize: 15728640, // this is the default (15 MB)
-  s3Options: {
-    accessKeyId: process.env.AWSAccessKeyId,
-    secretAccessKey: process.env.AWSSecretKey
-  }
-});
+s3 = new AWS.S3();
 
 var uploader = function(req, res) {
-  var blobUrl = req.body.file;
-  var zeKey = req.body.key;
- 
-  console.log('blobUrl is ', blobUrl);
-  var xhr = new XMLHttpRequest();
-  xhr.responseType = "blob";
+  console.log("REQ.FILES.FILE", req.files);
+  // console.log('REQ.BODY.KEY', req.body.key)
+  // console.log('Object.getPrototypeOf(req.body.file)', Object.getPrototypeOf(req.body.file));
+//   function toArrayBuffer(buf) {
+//     var ab = new ArrayBuffer(buf.length);
+//     var view = new Uint8Array(ab);
+//     for (var i = 0; i < buf.length; ++i) {
+//         view[i] = buf[i];
+//     }
+//     return ab;
+// }
 
-  xhr.onload = function() {
-    console.log("got into xhr.onload");
-    var recoveredBlob = this.response;
-    console.log('recovered blob is ', recoveredBlob);
-    var reader = new FileReader();
-    reader.onload = function() {
-      var blobAsDataUrl = reader.result;
-      var params = {
-        localFile: blobAsDataUrl,
+//   var arrBuff = toArrayBuffer(req.body.file.data);
 
-        s3Params: {
-          Bucket: "cloudmix",
-          Key: zeKey
-        }
-      };
-      console.log("got into reader.onload");
+//   function toBuffer(ab) {
+//     var buf = new Buffer(ab.byteLength);
+//     var view = new Uint8Array(ab);
+//     for (var i = 0; i < buf.length; ++i) {
+//         buf[i] = view[i];
+//     }
+//     return buf;
+// }
 
-      var upload = client.uploadFile(params);
+// var buffer = toBuffer(arrBuff);
 
-      upload.on("error", function(err) {
-        console.error("unable to upload: ", err.stack);
-      });
-
-      upload.on("progress", function() {
-        console.log(
-          "progress",
-          upload.progressMd5Amount,
-          upload.progressAmount,
-          upload.progressTotal
-        );
-      });
-
-      upload.on("end", function() {
-        console.log("done uploading");
-      });
-    };
-    
-    reader.readAsDataURL(recoveredBlob);
+  var params = {
+    Body: req.body.file.data,
+    Bucket: "cloudmix",
+    Key: "AlexTest.mp3",
+    ContentEncoding: "application/byte-stream"
   };
-  xhr.open("GET", blobUrl, true);
-  xhr.send();
+
+  s3.putObject(params, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("successful upload");
+    }
+  });
 };
 
 module.exports = {
