71b79bfd65fec30c1ba16336892cf0b216336cc1
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-05-04 15:45:12 -0700

    added play button onsong card & modularizeed song list entry to avoid repeat code

--- public/src/SongList/SongListEntry.js
+++ public/src/SongList/SongListEntry.js
@@ -1,42 +1,22 @@
 import React from "react";
 import {Link} from 'react-router-dom';
-import { Card, CardTitle } from "react-materialize";
+import SongCard from './SongCard.js';
 
 var SongListEntry = props => {
-  let status = ''
-  props.song.cont === 0 ? status = `Completed` : status = `Only ${props.song.cont} jam left`;
   return props.isLoggedIn ? (
     <Link to="/jam">
-      <Card
-        className="small"
-        header={
-          <CardTitle image="http://s.bellevuecollege.edu/kbcs/uploads/2013/03/Waveform-Transmission.jpg">
-            {props.song.title}
-          </CardTitle>
-        }
-        onClick={() => {
-          props.setSongTitle(props.song.title);
-          props.setGenre(props.song.genre);
-        }}
-      >
-        <b>Genre:</b> {props.song.genre} <br />
-        <b>{status}</b><br />
-        <b>Started by:</b> {props.song.creator}
-      </Card>
+      <SongCard
+        song={props.song}
+        setSongTitle={props.setSongTitle}
+        setGenre={props.setGenre}
+      />
     </Link>
   ) : (
-    <Card
-      className="small"
-      header={
-        <CardTitle image="http://s.bellevuecollege.edu/kbcs/uploads/2013/03/Waveform-Transmission.jpg">
-          {props.song.title}
-        </CardTitle>
-      }
-    >
-      <b>Genre:</b> {props.song.genre} <br />
-      <b>{status}</b><br />
-      <b>Started by:</b> {props.song.creator}
-    </Card>
+    <SongCard
+      song={props.song}
+      setSongTitle={props.setSongTitle}
+      setGenre={props.setGenre}
+    />
   );
 };
 
