35c6d29e8f772c61bb07f5b16bfbcb6c823baef4
David De Sousa <Sankhara@Davids-iMac.local>
2017-05-05 15:43:49 -0700

    Redirects and alerts on a successful upload

--- public/src/Jam/JamView.js
+++ public/src/Jam/JamView.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
-import WaveformVisual from './WaveformVisual.js'
-import getUserMedia from '../../audioHelpers/getUserMedia.js';
-import stopRecord from '../../audioHelpers/stopRecord.js';
-import record from '../../audioHelpers/record.js';
-import play from '../../audioHelpers/play.js';
-import stopPlayback from '../../audioHelpers/stopPlayback.js';
-import waveformInit from '../../audioHelpers/waveformInit.js';
-import inputVisual from '../../audioHelpers/inputVisual.js';
-import { Button } from "react-materialize";
-import axios from 'axios';
-import blobUtil from 'blob-util';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import WaveformVisual from "./WaveformVisual.js";
+import getUserMedia from "../../audioHelpers/getUserMedia.js";
+import stopRecord from "../../audioHelpers/stopRecord.js";
+import record from "../../audioHelpers/record.js";
+import play from "../../audioHelpers/play.js";
+import stopPlayback from "../../audioHelpers/stopPlayback.js";
+import waveformInit from "../../audioHelpers/waveformInit.js";
+import inputVisual from "../../audioHelpers/inputVisual.js";
+import { Button, Modal } from "react-materialize";
+import axios from "axios";
+import blobUtil from "blob-util";
 
 class JamView extends Component {
   constructor() {
@@ -26,9 +27,9 @@ class JamView extends Component {
       wavesurfer: null,
       blob: null,
       uploadSuccessful: false
-    }
-    
-    this.postBlobToDB= this.postBlobToDB.bind(this);
+    };
+
+    this.postBlobToDB = this.postBlobToDB.bind(this);
   }
 
   componentDidMount() {
@@ -43,14 +44,13 @@ class JamView extends Component {
   }
 
   setBlob(blob) {
-    this.setState({blob: blob});
+    this.setState({ blob: blob });
   }
 
-  postBlobToDB(){
-    if(this.state.urls.length < 2){
-    blobUtil.blobToBinaryString(this.state.blob)
-      .then(data => {
-        axios.post('/songs', {
+  postBlobToDB() {
+    if (this.state.urls.length < 2) {
+      blobUtil.blobToBinaryString(this.state.blob).then(data => {
+        axios.post("/songs", {
           title: this.props.songCreateTitle,
           genre: this.props.songCreateGenre,
           contriblimit: this.props.songCreateContributorLimit,
@@ -61,162 +61,167 @@ class JamView extends Component {
       });
       this.setState({
         uploadSuccessful: true
-      })
-      } else {
-        blobUtil.blobToBinaryString(this.state.blob)
-          .then(data => {
-            axios.put('/songs', {
-              contributors: this.props.currentUser,
-              url: this.state.urls[this.state.urls.length - 1],
-              offsets: this.state.offsets[this.state.offsets.length - 1]
-            });
-          });
-          this.setState({
-            uploadSuccessful: true
-          });
-      }
+      });
+      this.props.handleSuccessfulUpload();
+
+    } else {
+      blobUtil.blobToBinaryString(this.state.blob).then(data => {
+        axios.put("/songs", {
+          contributors: this.props.currentUser,
+          url: this.state.urls[this.state.urls.length - 1],
+          offsets: this.state.offsets[this.state.offsets.length - 1]
+        });
+      });
+      this.setState({
+        uploadSuccessful: true
+      });
+      this.props.handleSuccessfulUpload();
+    }
   }
 
-  handleOnSongSubmitClick(){
+  handleOnSongSubmitClick() {
     e.preventDefault();
     this.postBlobToDB();
   }
 
   setRecorder(recorder) {
-    this.setState({recorder: recorder});
+    this.setState({ recorder: recorder });
   }
 
   setRecording() {
-    this.setState({recording: !this.state.recording});
+    this.setState({ recording: !this.state.recording });
   }
 
   setUrl(url) {
     this.state.urls.push(url);
-    this.setState({change: !this.state.change});
+    this.setState({ change: !this.state.change });
   }
 
   setOffset(offset) {
-    this.setState({offset: offset});
+    this.setState({ offset: offset });
   }
 
   setTrackOffset(i, offset) {
     this.state.urls[i].offset = offset;
-    this.setState({change: !this.state.change});
+    this.setState({ change: !this.state.change });
   }
 
   setFirstRec() {
-    this.setState({firstRec: !this.state.firstRec});
+    this.setState({ firstRec: !this.state.firstRec });
   }
 
   setPlaying() {
-    this.setState({playing: !this.state.playing});
+    this.setState({ playing: !this.state.playing });
   }
 
   setTrack(track) {
     this.state.tracks.push(track);
-    this.setState({change: !this.state.change});
+    this.setState({ change: !this.state.change });
   }
 
   setWavesurfer(wave) {
-    this.setState({wavesurfer: wave});
+    this.setState({ wavesurfer: wave });
   }
 
   render() {
-    console.log(this.state.uploadSuccessful)
     return (
       <div className="jamView">
         <div className="jam_header">
-          <h3><b>Title:  </b>{this.props.songCreateTitle}</h3>
-          <h3><b>Genre:  </b>{this.props.songCreateGenre}</h3>
+          <h3><b>Title: </b>{this.props.songCreateTitle}</h3>
+          <h3><b>Genre: </b>{this.props.songCreateGenre}</h3>
         </div>
 
-        <div id="input"></div>
+        <div id="input" />
 
-        {this.state.uploadSuccessful === true ?
-          (
-            <div className="submittedBtn">
-              <h1>Successfully Uploaded Track</h1>
-            </div>
-          ) :
-          (
-            <div className="hideSubmitted">
+        {this.state.recording
+          ? <Button
+              className="red"
+              floating
+              large
+              waves="light"
+              icon="mic_off"
+              onClick={() => {
+                stopRecord(this.state.recorder, this.setRecording.bind(this));
+              }}
+            />
+          : <Button
+              className="recordButton"
+              floating
+              large
+              waves="light"
+              icon="mic_none"
+              onClick={() => {
+                record(
+                  this.state.recorder,
+                  this.state.urls,
+                  this.setRecording.bind(this),
+                  this.state.firstRec,
+                  this.setFirstRec.bind(this)
+                );
+              }}
+            />}
 
-            </div>
-          )
-        }
+        {!this.state.playing
+          ? <Button
+              floating
+              large
+              waves="light"
+              icon="play_arrow"
+              onClick={() => {
+                play(
+                  this.state.urls,
+                  this.state.offset,
+                  this.setTrackOffset.bind(this),
+                  this.setPlaying.bind(this),
+                  this.setTrack.bind(this)
+                );
+              }}
+            />
+          : <Button
+              floating
+              large
+              waves="light"
+              icon="stop"
+              onClick={() => {
+                stopPlayback(this.state.tracks, this.setPlaying.bind(this));
+              }}
+            />}
+        
+        {this.state.blob
+              ? 
+                  <Link to="/"><Button
+                    className="submitButton"
+                    onClick={e => this.postBlobToDB(e)}
+                  >
+                    Submit
+                  </Button></Link>
+              : <div />
+          }
+        
 
+        {this.state.urls > 1
+          ? <input
+              onChange={e => {
+                this.setOffset(e.target.value);
+                console.log("current offset is", e.target.value);
+              }}
+            />
+          : null}
 
-        {this.state.recording ? (
-          <Button className='red' floating large waves='light' icon='mic_off'
-            onClick={() => {
-              stopRecord(this.state.recorder, this.setRecording.bind(this));
-            }}
-          ></Button>
-        ) : (
-          <Button className='recordButton'  floating large waves='light' icon='mic_none'
-            onClick={() => {
-              record(
-                this.state.recorder,
-                this.state.urls,
-                this.setRecording.bind(this),
-                this.state.firstRec,
-                this.setFirstRec.bind(this)
-              );
-            }}
-          ></Button>
-        )}
-
-        {!this.state.playing ? (
-          <Button
-            floating large
-            waves='light'
-            icon='play_arrow'
-            onClick={() => {
-              play(
-                this.state.urls,
-                this.state.offset,
-                this.setTrackOffset.bind(this),
-                this.setPlaying.bind(this),
-                this.setTrack.bind(this)
-              );
-            }}
-          ></Button>
-        ) : (
-          <Button
-            floating large
-            wave="light"
-            icon="stop"
-            onClick={() => {
-              stopPlayback(this.state.tracks, this.setPlaying.bind(this));
-            }}
-          ></Button>
-        )}
-
-        {this.state.blob ? ( <Button className="submitButton" onClick={(e) => this.postBlobToDB(e)}>
-          Submit
-        </Button> ) : null }
-
-        {this.state.urls > 1 ? (
-          <input onChange={e => {
-            this.setOffset(e.target.value);
-            console.log('current offset is', e.target.value);
-          }}></input>
-        ) : null}
-
-        <div className="waveform"></div>
+        <div className="waveform" />
         {this.state.urls.map((url, i) => {
-          return <WaveformVisual
-            key={i}
-            url={url.url}
-            wavesurfer={this.state.wavesurfer}
-          />
+          return (
+            <WaveformVisual
+              key={i}
+              url={url.url}
+              wavesurfer={this.state.wavesurfer}
+            />
+          );
         })}
 
-
-    </div>
+      </div>
     );
   }
 }
-
 
 export default JamView;
